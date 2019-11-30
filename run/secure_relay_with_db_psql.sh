d9329e097131c0b4f582c7a37ad66b2d72fb3f87
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-11-30 14:37:55 +0530

    Update secure_relay_with_db_psql.sh

--- run/secure_relay_with_db_psql.sh
+++ run/secure_relay_with_db_psql.sh
@@ -3,14 +3,14 @@
 turnserver --server-name="${TURN_SERVER_FQDN}" \
   --verbose \ 
   --syslog \
-  --lt-cred-mech  
-  --max-bps=3000000 \
+  --lt-cred-mech \
+  --max-bps=${MAX_BPS}\
   --fingerprint \
-  ---relay-threads 3 \
-  --min-port=49152 --max-port=65535 \
+  ---relay-threads ${RELAY_THREADS} \
+  --min-port=${MIN_PORT} --max-port=${MAX_PORT} \
   --realm ${REALM} \
   --psql-userdb="host=postgresql dbname=coturn user=coturn password=${PSQL_DB_PASSWORD} connect_timeout=30" \
-  --cert=/etc/ssl/le/cert.pem --pkey=/etc/ssl/privkey.pem \
+  --cert=/etc/ssl/le/cert.pem --pkey=/etc/ssl/le/privkey.pem \
   --log-file=stdout \
   --cipher-list=ALL \
   --oauth \
