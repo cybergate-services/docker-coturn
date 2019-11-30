5dc77766ec5011b6fc72f5d13e597d490058b69b
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-11-30 13:40:39 +0530

    Update secure_relay_with_db_psql.sh

--- run/secure_relay_with_db_psql.sh
+++ run/secure_relay_with_db_psql.sh
@@ -34,7 +34,7 @@ turnserver --server-name="blackdow.carleon.gov" \
   --fingerprint \
   ---relay-threads 3 \
   --min-port=49152 --max-port=65535 \
-  -r north.gov \
+  --realm ${REALM} \
   --psql-userdb="host=postgresql dbname=coturn user=coturn password=${PSQL_DB_PASSWORD} connect_timeout=30" \
   --cert=/etc/ssl/le/cert.pem --pkey=/etc/ssl/privkey.pem \
   --log-file=stdout \
