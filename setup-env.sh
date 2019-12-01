c1470e609505229f81b6bce54c9a0869e345946d
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-01 21:36:32 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 rm -rf ./.env
-PASSWORD=$(openssl rand -base64 16)
+PASSWORD=$(openssl rand -hex 16)
+TURN_USER_SECRET=$(openssl rand -hex 16)
 
 cat coturn/coturn.env > ./.coturn.env
-sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.coturn.env               
+sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.coturn.env
+sed -i -e "s/CHANGE_MY_SECRET/$TURN_USER_SECRET/g" ./.coturn.env               
 
 cat postgresql/postgresql.env > ./.postgresql.env
 sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.postgresql.env
